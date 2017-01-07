1241190495b20617584b3d4485b3494075fdaf20
TheCGaming <tiidezorder@yahoo.com>
2017-01-07 19:04:39 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
-# spectate
-A simple spectator plugin for MCPE servers
-
-Commands:
-  main-command: /spectate
-        sub-commands: on , off
-        
-/spectate on puts you in spectator mode
-/spectate off disabled spectator mode
-
-*
-I MADE THIS PLUGIN SO THAT WHEN PLAYERS JOIN, THEIR GAMEMODE WILL BE RESET TO SURVIVAL!
+# Nick
+Nick Plugin - Set a nick-name for yourself, by running the command /nick set.
---
+++ src/spc/Main.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace spc;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener {
+    
+    public $nicknames = [];
+
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+    }
+
+    public function onJoin(PlayerJoinEvent $e){
+        $p = $e->getPlayer();
+        if(isset($this->nicknames[strtolower($p->getName())])){
+            $name = $this->nicknames[strtolower($p->getName())];
+			$p->setGamemode(0);
+            $p->sendMessage(TextFormat::GREEN."");
+        }
+    }
+   
+    public function onCommand(CommandSender $sender, Command $cmd, $list, array $args){
+        switch($cmd){
+            case "spectate":
+                if(count($args) != 1){
+                    $sender->sendMessage(TextFormat::RED."Usage: /spectate on/off");
+                    return;
+                }
+                if(strtolower($args[0]) == "off"){
+                    $sender->setGamemode(0);
+                    $sender->sendMessage("§bYou are no longer spectating!");
+                    return;
+                }
+                if(strtolower($args[0]) == "on"){
+                    $sender->setGamemode(3);
+                        $sender->sendMessage(TextFormat::RED."§bYou are now spectating! 
+						Use /spectate off to stop spectating");
+            break;
+       }
+    }
+}
+}
